9b44e021400e2d7ff087d9b5d43f09d1a82b76ba
Tirdea Mihai <tirdea.mihai@gmail.com>
2017-05-15 02:27:05 +0300

    conversation in menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,10 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+
+                            <li class="dropdown">
+                                <a class="dropdown-toggle" href="{{ route('conversations.index') }}"> Conversations</a>
+                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
